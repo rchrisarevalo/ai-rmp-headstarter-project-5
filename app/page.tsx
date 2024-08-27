3b0753126511b077d82236f89c8efe70bf39dcdc
rchrisarevalo <rubenplayer105@gmail.com>
2024-08-27 12:56:13 -0500

    Additional configurations made

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import DashboardForm from "./components/DashboardForm";
 import { UserRes, BotRes } from "./components/Responses";
 import { ChatMessage } from "./types/types.config";
 import "@fontsource/source-sans-pro";
-import Nav from "./components/Nav";
 import { IoIosBookmarks } from "react-icons/io";
 import ParticleBackground from "./components/ParticleBackground";
 
@@ -14,7 +13,7 @@ export default function Home() {
     {
       role: "assistant",
       content:
-        "Hi there! My name is **AI Rate My Professor**, and I will be your assistant today! How can I help you out in this fine day?",
+        "Hi there! My name is **RateTeach AI**, and I will be your assistant today! How can I help you out in this fine day?",
     },
   ]);
 
@@ -28,12 +27,11 @@ export default function Home() {
         }}
         className="flex min-h-screen flex-col items-center justify-evenly p-24 bg-transparent text-black"
       >
-        {/* <Nav /> */}
         <h1 className="text-black items-center justify-center font-['Source_Sans_Pro'] flex flex-row gap-x-2 text-4xl font-bold">
           <p className="text-red">
             <IoIosBookmarks color='red' />
           </p>
-          AI Rate My Professor
+          RateTeach AI
         </h1>
         <div className="shadow-xl flex flex-col font-['Source_Sans_Pro'] w-[60%] mt-16 text-lg items-center max-sm:w-screen justify-evenly p-12 rounded-lg bg-slate-300 gap-12">
           <Dashboard>
